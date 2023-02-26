84445c553daf67a05ab56b19473717f81a2b3f91
Lukas <lukascrypto2006@gmail.com>
2023-02-25 23:19:02 -0300

    first commit

---
+++ src/public/js/main.js
@@ -0,0 +1,72 @@
+$(function() {
+    const socket = io();
+    var nick = '';
+
+    const messageForm = $('#message-form');
+    const messageBox = $('#message');
+    const chat = $('#chat');
+
+    const nickForm = $('#nick-form');
+    const nickError = $('#nick-error');
+    const nickName = $('#nick-name');
+
+    const userNames = $('#usernames');
+
+    messageForm.submit(e => {
+        e.preventDefault();
+        socket.emit('enviar mensaje', messageBox.val());
+        messageBox.val('');
+    })
+
+    socket.on('nuevo mensaje', function(datos) {
+        // console.log(datos)
+
+        let color = "#f4f4f4";
+        if(nick === datos.username) {
+            color = "#9ff4c5";
+        }
+
+        chat.append(`<div class="msg-area mb-2 d-flex" style="background-color:${color}"><b>${datos.username}:</b><p class="msg"> ${datos.msg}</p></div>`);
+
+    });
+
+    nickForm.submit(e => {
+        e.preventDefault();
+
+        socket.emit('nuevo usuario', nickName.val(), datos => {
+            if(datos) {
+                nick = nickName.val();
+                $('#nick-wrap').hide();
+                $('#content-wrap').show()
+            } else {
+                nickError.html('<div class="alert alert-danger">El usuario ya existe</div>')
+            }
+
+            nickName.val('')
+        })
+    })
+
+    socket.on('nombre usuario', datos => {
+        let html = '';
+        let color = '';
+        let salir = '';
+
+        for(let i = 0; i < datos.length; i++) {
+            if(nick == datos[i]) {
+                color = '#027F43';
+                salir = '<a class="enlace-salir" href="/">Salir</a>'
+            } else {
+                color = '#000'
+            }
+
+            html += `<p style="color: ${color}">${datos[i]} ${salir}</p>`;
+        }
+
+        userNames.html(html)
+    })
+
+
+
+})
+
+
---
+++ src/socket.js
@@ -0,0 +1,37 @@
+module.exports = (io) => {
+
+    let nickNames = [];
+
+    io.on('connection', socket => {
+        console.log('Nuevo usuario conectado');
+
+        socket.on('enviar mensaje', (datos) => {
+            io.sockets.emit('nuevo mensaje', {
+                msg:datos,
+                username: socket.nickname
+            })
+        })
+
+        socket.on('nuevo usuario', (datos, callback) => {
+            if(nickNames.indexOf(datos) != -1) {
+                callback(false);
+            } else {
+                callback(true);
+                socket.nickname = datos;
+                nickNames.push(socket.nickname);
+
+                io.sockets.emit('nombre usuario', nickNames);
+            }
+        })
+
+        socket.on("disconnect", datos => {
+            if(!socket.nickname) {
+                return;
+            } else {
+                nickNames.splice(nickNames.indexOf(socket.nickname), 1);
+                io.sockets.emit('nombre usuario', nickNames);
+            }
+        })
+    })
+
+}
